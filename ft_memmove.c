58d0250d8e6e4998d1befcd83717d64bcdafeac7
Samujesu05 <samujesu.borre@gmail.com>
2023-09-28 20:26:05 +0200

    memmove casi terminado

--- ft_memmove.c
+++ ft_memmove.c
@@ -12,7 +12,23 @@
 
 #include "libft.h"
 
-void    *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	
+	unsigned const char	*source;
+	unsigned char		*destination;
+	size_t				i;
+
+	source = (unsigned char *)src;
+	destination = (unsigned char *)dest;
+	i = 0;
+	while (dest == src || !n)
+	{
+		return (dest);
+	}
+	while (n > i)
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	return (dest);
 }
