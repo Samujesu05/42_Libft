2c2fc053be7402e48d505e5e4c205175008e92d7
Samujesu05 <samujesu.borre@gmail.com>
2023-09-30 19:32:53 +0200

    strlcpy y cat terminado

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,34 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sborrego <sborrego@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/30 14:22:02 by sborrego          #+#    #+#             */
-/*   Updated: 2023/09/30 14:22:02 by sborrego         ###   ########.fr       */
+/*   Created: 2023/09/30 19:32:31 by sborrego          #+#    #+#             */
+/*   Updated: 2023/09/30 19:32:31 by sborrego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	unsigned int	i;
-	unsigned int	srclen;
-
-	i = 0;
-	srclen = ft_strlen(src);
-	if (!dst || !src)
-		return (0);
-	if (dstsize != 0)
-	{
-		while (src[i] != '\0' && i < dstsize - 1)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (srclen);
-}
