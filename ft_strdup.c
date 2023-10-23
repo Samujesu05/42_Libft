42b492a87c157b895669be3ce4aca862311c7c08
Samujesu05 <samujesu.borre@gmail.com>
2023-10-23 09:16:33 +0200

    Todos menos adicionales y listas

---
+++ ft_strdup.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sborrego <sborrego@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/23 08:54:53 by sborrego          #+#    #+#             */
+/*   Updated: 2023/10/23 08:54:53 by sborrego         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	int		i;
+	int		size;
+
+	size = 0;
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+	str = malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
