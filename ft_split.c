29986ed9ed88680263e2706a1e61be3d2a74da6d
Samujesu05 <samujesu.borre@gmail.com>
2023-10-23 08:48:36 +0200

    Calloc terminado

--- ft_split.c
+++ ft_split.c
@@ -10,11 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stddef.h>
 #include "libft.h"
 
 static char	*ft_word_len(char *s, char c)
