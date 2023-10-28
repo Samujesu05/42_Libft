e59bc91b5ede87c5a3ad9fdbc3bfc5c995cf48c0
Samujesu05 <samujesu.borre@gmail.com>
2023-10-28 16:07:38 +0200

    Casi todo

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,33 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sborrego <sborrego@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sborrego <sborrego@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/26 19:10:25 by sborrego          #+#    #+#             */
-/*   Updated: 2023/10/26 19:10:43 by sborrego         ###   ########.fr       */
+/*   Created: 2023/10/28 16:02:35 by sborrego          #+#    #+#             */
+/*   Updated: 2023/10/28 16:02:35 by sborrego         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	char	*aux;
-	int		i;
+	int	i;
 
-	if (!s || !f)
-		return (NULL);
-	aux = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
-	if (!aux)
-		return (NULL);
 	i = 0;
+	if (!s || !f)
+		return ;
 	while (s[i])
 	{
-		aux[i] = f(i, s[i]);
+		f(i, &(s[i]));
 		++i;
 	}
-	aux[i] = '\0';
-	return (aux);
 }
